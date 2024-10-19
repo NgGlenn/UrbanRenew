fbbf871dad7dcfdde84f5ac21c96bc03ceb730b4
JohnBackflip <jotayoer@gmail.com>
2024-10-19 13:12:04 +0800

    Navbar Changes
    Changed NavBar to shaun's design
    Added css style for button
    Added my own code

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 //Firestore
-import {db, auth} from './firebase.js';
+//import {db, auth} from './firebase.js';
 
 const app = createApp(App)
 
-app.provide('db', db)
-app.provide('auth', auth)
+//app.provide('db', db)
+//app.provide('auth', auth)
 app.use(router)
 
 
